4ed318cc153312c35867d7c12cd8dbda6e885355
Kauan <cvitorcordeiro@gmail.com>
2024-07-07 20:26:21 -0300

    first commmit

---
+++ android/app/src/main/kotlin/dev/kauancordeiro/todo_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.kauancordeiro.todo_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  TodoListPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
