06e7d63c32c4216ed655ccc6f9119e760a1dda7b
Kauan <cvitorcordeiro@gmail.com>
2024-07-07 21:08:57 -0300

    txtfield and add button

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -3,12 +3,50 @@ import 'package:flutter/material.dart';
 class TodoListPage extends StatelessWidget {
   TodoListPage({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            children: [
+              const Expanded(
+                flex: 3,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Adicione uma tarefa',
+                    hintText: 'Ex. Estudar Flutter',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: add,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff00d7f3),
+                  iconColor: Colors.white,
+                  padding: const EdgeInsets.all(15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  )
+                ),
+                child: const Icon(
+                  Icons.add,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
+  void add() {
+    print("ADD");
+  }
 }
